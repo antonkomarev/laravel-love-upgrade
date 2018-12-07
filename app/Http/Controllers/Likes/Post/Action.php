1ae905806d206493f5f6428c2a20b3d413de16d3
a-komarev <ell@cybercog.su>
2018-12-07 11:28:52 +0300

    Use only custom models in app

--- app/Http/Controllers/Likes/Post/Action.php
+++ app/Http/Controllers/Likes/Post/Action.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers\Likes\Post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Author;
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Wink\WinkAuthor;
 
 class Action extends Controller
 {
     public function __invoke(Request $request)
     {
-        $liker = WinkAuthor::query()->whereKey($request->input('author_id'))->firstOrFail();
+        $liker = Author::query()->whereKey($request->input('author_id'))->firstOrFail();
         $likeable = Post::query()->whereKey($request->input('post_id'))->firstOrFail();
 
         try {
