b3388c3047cc4d0de2ed52f3d9758308fa641313
a-komarev <ell@cybercog.su>
2018-12-19 16:20:25 +0300

    Fix favorite posts filtering

--- app/Http/Controllers/FavoritePosts/Collect/Action.php
+++ app/Http/Controllers/FavoritePosts/Collect/Action.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\FavoritePosts\Collect;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Cog\Laravel\Love\ReactionType\Models\ReactionType;
 use Illuminate\Http\Request;
 
 class Action extends Controller
@@ -11,7 +12,7 @@ class Action extends Controller
     public function __invoke(Request $request)
     {
         $posts = Post::query()
-            ->whereReactedBy($request->user()->getReacter())
+            ->whereReactedWithTypeBy($request->user()->getReacter(), ReactionType::fromName('Like'))
             ->with('tags', 'reactant.reactions.reacter.reacterable', 'reactant.reactions.type')
             ->live()
             ->orderBy('publish_date', 'DESC')
